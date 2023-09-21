8dac5a239bde8e3306d9d04928fbe57565b3c004
BNW0 <107748401+BNW0@users.noreply.github.com>
2023-09-21 11:18:08 +0000

    Use shorter notation

--- src/main/java/eu/crydee/syllablecounter/SyllableCounter.java
+++ src/main/java/eu/crydee/syllablecounter/SyllableCounter.java
@@ -1,4 +1,5 @@
 /*
  * Copyright 2014 Hugo “m09” Mougard
  * 
- * Licensed under the Apache License, Version 2.0 (the "Licens
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file exc
