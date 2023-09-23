ef5a7b7913be001f650bc22ace34e145ff58d6e2
BNW0 <107748401+BNW0@users.noreply.github.com>
2023-09-23 20:15:55 +0000

    Re-generated Makefiles

--- src/main/java/eu/crydee/syllablecounter/SyllableCounter.java
+++ src/main/java/eu/crydee/syllablecounter/SyllableCounter.java
@@ -2,4 +2,4 @@
  * Copyright 2014 Hugo “m09” Mougard
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file exc
+ * you may not use this file except in compliance with the License
