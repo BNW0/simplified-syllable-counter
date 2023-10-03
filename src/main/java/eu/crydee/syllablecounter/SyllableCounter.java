4fe5b3a60c6d92c312dd082504ee8739e14bca1a
BNW0 <107748401+BNW0@users.noreply.github.com>
2023-10-03 17:43:35 +0000

    Regenerate files

--- src/main/java/eu/crydee/syllablecounter/SyllableCounter.java
+++ src/main/java/eu/crydee/syllablecounter/SyllableCounter.java
@@ -11,4 +11,6 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitatio
+ * limitations under the License.
+ */
+package e
