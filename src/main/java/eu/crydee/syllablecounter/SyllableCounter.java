4d08298aca7e56fea1f6e13ccff6669417e17cd1
BNW0 <107748401+BNW0@users.noreply.github.com>
2023-10-05 11:37:56 +0000

    updating views

--- src/main/java/eu/crydee/syllablecounter/SyllableCounter.java
+++ src/main/java/eu/crydee/syllablecounter/SyllableCounter.java
@@ -19,4 +19,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import jav
+import java.util.Arrays;
+import java.util.HashSet;
+import
