4c73ad4c9687c137e45dc68da7f95e905238aca9
BNW0 <107748401+BNW0@users.noreply.github.com>
2023-10-05 12:06:03 +0000

    Fix a broken link

--- src/main/java/eu/crydee/syllablecounter/SyllableCounter.java
+++ src/main/java/eu/crydee/syllablecounter/SyllableCounter.java
@@ -21,4 +21,6 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.HashSet;
-import
+import java.util.Locale;
+import java.util.Map;
+impor
