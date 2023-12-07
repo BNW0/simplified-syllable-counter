38e51ab9b203539e68f209dc2fbdd9c7fc89eda4
BNW0 <107748401+BNW0@users.noreply.github.com>
2023-12-07 08:12:56 +0000

    Update python_3

--- src/main/java/eu/crydee/syllablecounter/SyllableCounter.java
+++ src/main/java/eu/crydee/syllablecounter/SyllableCounter.java
@@ -83,4 +83,4 @@ public class SyllableCounter {
                 .collect(Collectors.toMap(
                         fields -> fields[1],
                         fields -> Integer.parseInt(fields[0])));
-        addSyls = getRe
+        addSyls = getRessourceLines(getClass(), ADDSYL_PATH)
