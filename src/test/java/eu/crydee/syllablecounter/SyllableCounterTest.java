e5777bc76cadf73407d0e9b94ffcb613979f2cf3
BNW0 <107748401+BNW0@users.noreply.github.com>
2024-01-30 18:50:07 +0000

    login

--- src/test/java/eu/crydee/syllablecounter/SyllableCounterTest.java
+++ src/test/java/eu/crydee/syllablecounter/SyllableCounterTest.java
@@ -2,4 +2,5 @@
  * Copyright 2015 m09.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file exce
+ * you may not use this file except in compliance with the License.
+ *
