6807b51d9ddae5b9962e9c9c01faa1b419ff667e
BNW0 <107748401+BNW0@users.noreply.github.com>
2024-01-30 18:21:29 +0000

    Removed files.

--- src/test/java/eu/crydee/syllablecounter/SyllableCounterTest.java
+++ src/test/java/eu/crydee/syllablecounter/SyllableCounterTest.java
@@ -1,4 +1,5 @@
 /*
  * Copyright 2015 m09.
  *
- * Licensed under the Apache License, Version 2.0 (the "License")
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file exce
