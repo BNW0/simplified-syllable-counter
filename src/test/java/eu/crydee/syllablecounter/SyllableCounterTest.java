0bffb97953020d19135ed75af55227f3193e86d9
BNW0 <107748401+BNW0@users.noreply.github.com>
2024-02-19 20:22:59 +0000

    More docstrings.

--- src/test/java/eu/crydee/syllablecounter/SyllableCounterTest.java
+++ src/test/java/eu/crydee/syllablecounter/SyllableCounterTest.java
@@ -13,4 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package eu.crydee.syllab
+package eu.crydee.syllablecounter;
+
+import org.junit.Test;
+
+import
