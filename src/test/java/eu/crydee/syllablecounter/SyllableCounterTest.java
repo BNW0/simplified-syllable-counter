b448172e56d2d5de1d99b43295f73476db376763
BNW0 <107748401+BNW0@users.noreply.github.com>
2024-03-26 09:52:43 +0000

    Removed unused import

--- src/test/java/eu/crydee/syllablecounter/SyllableCounterTest.java
+++ src/test/java/eu/crydee/syllablecounter/SyllableCounterTest.java
@@ -65,4 +65,4 @@ public class SyllableCounterTest {
 
     private void testFromFile(String filepath) {
         SyllableCounter sc = new SyllableCounter();
-        sc.getRessourceLines(getClass(), filepath).filter(line -> !
+        sc.getRessourceLines(getClass(), filepath).filter(line -> !line.isEmpty()).filter(line -> !line.start
