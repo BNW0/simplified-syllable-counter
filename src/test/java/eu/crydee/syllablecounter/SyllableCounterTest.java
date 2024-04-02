3e720bb3b80eac9807e5c38b7e8f756ccab6d20a
BNW0 <107748401+BNW0@users.noreply.github.com>
2024-04-02 09:07:28 +0000

    flakify

--- src/test/java/eu/crydee/syllablecounter/SyllableCounterTest.java
+++ src/test/java/eu/crydee/syllablecounter/SyllableCounterTest.java
@@ -69,4 +69,5 @@ public class SyllableCounterTest {
                 .forEach(line -> {
                     String[] fields = line.split(" ");
                     if (fields.length != 2) {
-                        System.err.println("coul
+                        System.err.println("couldn't parse " + filepath + ". Didn't "
+       
